f717319184b7899a09defe62b6b302181f622a54
dawson <xmen@uwaterloo.ca>
2021-11-02 10:59:24 -0400

    After refactoring (extracting method) of "Mortgage calculator", to keep the code in a method 5 - 10 lines.

--- src/com/codewithdawson/mortgagecalculator/MortgageCalculator.java
+++ src/com/codewithdawson/mortgagecalculator/MortgageCalculator.java
@@ -12,18 +12,26 @@ public class MortgageCalculator {
         float annualPercentInterestRate = (float) readNumber("Annual Interest Rate: ", 1,30);
         byte years = (byte)readNumber("Period (Years): ",1,30);
 
-        double mortgage = calculateMortgage(principal,annualPercentInterestRate,years);
+        printMortgage(principal, annualPercentInterestRate, years);
+
+        printPaymentSchedule(principal, annualPercentInterestRate, years);
+    }
+
+    private static void printMortgage(int principal, float annualPercentInterestRate, byte years) {
+        double mortgage = calculateMortgage(principal, annualPercentInterestRate, years);
         String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
         System.out.println();
         System.out.println("MORTGAGE");
         System.out.println("---------");
         System.out.println("Monthly Payments: " + mortgageFormatted);
+    }
 
+    private static void printPaymentSchedule(int principal, float annualPercentInterestRate, byte years) {
         System.out.println();
         System.out.println("PAYMENT SCHEDULE");
         System.out.println("------------------");
         for (short month = 1; month <= years * MONTHS_IN_YEAR; month++) {
-            double balance = calculateBalance(principal, annualPercentInterestRate,years,month);
+            double balance = calculateBalance(principal, annualPercentInterestRate, years,month);
             System.out.println(NumberFormat.getCurrencyInstance().format(balance));
         }
     }
